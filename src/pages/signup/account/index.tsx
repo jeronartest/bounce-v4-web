e89f60cc91f4e3ff5c2611d0b59189eb02d76244
bill <>
2023-03-24 13:12:41 +0800

    feat: update signup

--- src/pages/signup/account/index.tsx
+++ src/pages/signup/account/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import * as yup from 'yup'
 import { Formik, Form } from 'formik'
-import { Box, IconButton, InputAdornment, OutlinedInput } from '@mui/material'
+import { Box, IconButton, InputAdornment, OutlinedInput, Typography } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import md5 from 'md5'
 import { ReactComponent as VisibilityOn } from 'assets/imgs/user/visibility_on.svg'
@@ -12,10 +12,14 @@ import LoginLayout from 'bounceComponents/signup/LoginLayout'
 import { ACCOUNT_TYPE, USER_TYPE } from 'api/user/type'
 import { useRegister } from 'bounceHooks/user/useRegister'
 import { ThirdParties } from 'bounceComponents/signup/ThirdParties'
-import { checkEmail } from 'api/user'
+import { checkEmail, verifyCode } from 'api/user'
 import { Link, useNavigate } from 'react-router-dom'
 import { routes } from 'constants/routes'
 import { stringify } from 'querystring'
+import { useCountDown, useRequest } from 'ahooks'
+import { toast } from 'react-toastify'
+import { ReactComponent as CodeSendSVG } from 'bounceComponents/profile/account/CodeSend.svg'
+import { isEmail } from 'utils'
 
 // export type IAccountProps = {}
 const validationSchema = yup.object({
@@ -30,6 +34,16 @@ const validationSchema = yup.object({
         return false
       }
       return true
+    }),
+  code: yup
+    .string()
+    .trim()
+    .required('Please enter your verification code')
+    .test('CODE', 'Incorrect verification code', function (val) {
+      if (val && /^[0-9]{6}$/.test(val)) {
+        return true
+      }
+      return false
     }),
   name: yup
     .string()
@@ -48,13 +62,23 @@ const validationSchema = yup.object({
 const Account: React.FC = ({}) => {
   const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState<boolean>(false)
+
+  const [btnDisable, setBtnDisable] = useState<boolean>(true)
+  const [showCountDown, setShowCountDown] = useState<number>()
+  const [countdown] = useCountDown({
+    targetDate: showCountDown,
+    onEnd: () => {
+      setShowCountDown(undefined)
+    }
+  })
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword)
   }
   const initialValues = {
     password: '',
     name: '',
-    email: ''
+    email: '',
+    code: ''
   }
   const { loading, runAsync: runRegister } = useRegister()
   const handleSubmit = (values: typeof initialValues) => {
@@ -68,13 +92,27 @@ const Account: React.FC = ({}) => {
     })
   }
 
+  const { run: sendVerifyCode } = useRequest(async email => verifyCode({ email: email }), {
+    manual: true,
+    onSuccess: response => {
+      const { code } = response
+      if (code !== 200) {
+        setShowCountDown(undefined)
+        return toast.error('Incorrect verification code')
+      } else {
+        setBtnDisable(false)
+        return toast.success('Send successfully')
+      }
+    }
+  })
+
   const handleOauth = (accessToken: string, oauthType: ACCOUNT_TYPE) => {
     navigate(routes.signup.thirdPartiesAccount + '?' + stringify({ accessToken, oauthType }))
   }
   return (
     <LoginLayout title={'Create Account'} subTitle={<Link to={routes.login}>Sign in</Link>}>
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
-        {() => (
+        {({ values }) => (
           <Box component={Form} noValidate>
             <FormItem name="email" label="Email" required>
               <OutlinedInput />
@@ -95,7 +133,42 @@ const Account: React.FC = ({}) => {
                 }
               />
             </FormItem>
-            <LoadingButton loading={loading} type="submit" fullWidth variant="contained" sx={{ mt: 32, mb: 32 }}>
+            <FormItem required name="code" sx={{ mt: 20 }} label="Email verification code">
+              <OutlinedInput
+                endAdornment={
+                  <InputAdornment position="end">
+                    {countdown === 0 ? (
+                      <Box
+                        sx={{ display: 'flex', cursor: 'pointer' }}
+                        onClick={() => {
+                          if (isEmail(values.email)) {
+                            sendVerifyCode(values.email)
+                            setShowCountDown(Date.now() + 60000)
+                          }
+                        }}
+                      >
+                        <Typography variant="body1" color={'#2663FF'} mr={6}>
+                          Send
+                        </Typography>
+                        <CodeSendSVG />
+                      </Box>
+                    ) : (
+                      <Typography variant="body1" color={'var(--ps-gray-900)'}>
+                        {Math.round(countdown / 1000)}s
+                      </Typography>
+                    )}
+                  </InputAdornment>
+                }
+              />
+            </FormItem>
+            <LoadingButton
+              loading={loading}
+              type="submit"
+              fullWidth
+              disabled={btnDisable}
+              variant="contained"
+              sx={{ mt: 32, mb: 32 }}
+            >
               Continue
             </LoadingButton>
           </Box>
