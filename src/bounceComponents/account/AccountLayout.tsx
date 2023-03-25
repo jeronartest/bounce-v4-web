f1a836db33e6dd1f643c457b2b7359dcbd32fdda
bill <>
2023-03-25 13:13:58 +0800

    feat: add account login check

--- src/bounceComponents/account/AccountLayout.tsx
+++ src/bounceComponents/account/AccountLayout.tsx
@@ -1,7 +1,20 @@
 import { Box } from '@mui/material'
+import { routes } from 'constants/routes'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useUserInfo } from 'state/users/hooks'
 import LeftMenu from './LeftMenu'
 
 export default function AccountLayout({ children }: { children: JSX.Element | string }) {
+  const { userId } = useUserInfo()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!userId) {
+      navigate(routes.market.index)
+    }
+  }, [navigate, userId])
+
   return (
     <Box
       sx={{
