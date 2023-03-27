15052f58553416836fd21d99d634adb3c73b91e1
bill <>
2023-03-27 18:37:34 +0800

    feat: update login

--- src/bounceComponents/signup/LoginLayout.tsx
+++ src/bounceComponents/signup/LoginLayout.tsx
@@ -2,6 +2,8 @@ import { Container, Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import React from 'react'
 import SignupLayout from './SignupLayout'
+import login_bg from 'assets/svg/login_bg.svg'
+
 export type ILoginLayoutProps = {
   title: string
   subTitle: React.ReactNode
@@ -12,29 +14,43 @@ const LoginLayout: React.FC<ILoginLayoutProps> = ({ title, subTitle, children })
   return (
     <SignupLayout>
       <Container maxWidth="xl">
-        <Box
-          sx={{
-            m: '0 auto',
-            width: 708,
-            borderRadius: 20,
-            background: '#FFFFFF',
-            pt: '80px',
-            pb: '66px'
-          }}
-        >
-          <Box sx={{ textAlign: 'center' }}>
-            <Stack spacing={'20px'} alignItems="center">
-              <Typography fontSize={'28px'} lineHeight="36px" variant="h1" color={'var(--ps-gray-900)'}>
-                {title}
-              </Typography>
-              <Typography color="var(--ps-gray-600)" display={'flex'}>
-                {title === 'Login' ? `Don't have an account yet? ` : 'Already have an account?'}
-                &nbsp;
-                <span style={{ color: '#2663FF' }}>{subTitle}</span>
-              </Typography>
-            </Stack>
-            <Box width={'428px'} m="0 auto" mt={'40px'}>
-              {children}
+        <Box display="grid" gridTemplateColumns={'1fr 1fr'}>
+          <Box padding={10}>
+            <Box
+              sx={{
+                background: `url(${login_bg}) no-repeat`,
+                backgroundColor: '#F5F5F5',
+                borderRadius: 24,
+                height: '100%',
+                backgroundPosition: 'center center'
+              }}
+            ></Box>
+          </Box>
+          <Box
+            sx={{
+              m: '0 auto',
+              maxWidth: 708,
+              height: 772,
+              borderRadius: 20,
+              background: '#FFFFFF',
+              pt: '60px',
+              pb: '60px'
+            }}
+          >
+            <Box sx={{ textAlign: 'center' }}>
+              <Stack spacing={'20px'} alignItems="center">
+                <Typography fontSize={'28px'} lineHeight="36px" variant="h1" color={'var(--ps-gray-900)'}>
+                  {title}
+                </Typography>
+                <Typography color="var(--ps-gray-600)" display={'flex'}>
+                  {title === 'Login' ? `Don't have an account yet? ` : 'Already have an account?'}
+                  &nbsp;
+                  <span style={{ color: '#2663FF' }}>{subTitle}</span>
+                </Typography>
+              </Stack>
+              <Box width={'428px'} m="0 auto" mt={'40px'}>
+                {children}
+              </Box>
             </Box>
           </Box>
         </Box>
--- src/bounceComponents/signup/SignupLayout.tsx
+++ src/bounceComponents/signup/SignupLayout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export type ISignupLayoutProps = { children?: React.ReactNode }
 
 const SignupLayout: React.FC<ISignupLayoutProps> = ({ children }) => {
-  return <Box sx={{ pt: 40 }}>{children}</Box>
+  return <Box>{children}</Box>
 }
 
 export default SignupLayout
