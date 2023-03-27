8265770fbf3b8752733c355f40daf9db0aee439c
bill <>
2023-03-27 23:20:29 +0800

    feat: hide linkedin

--- src/bounceComponents/profile/account/components/LoginOption/index.tsx
+++ src/bounceComponents/profile/account/components/LoginOption/index.tsx
@@ -1,20 +1,19 @@
 import { Stack } from '@mui/system'
 import React from 'react'
 import SettingsBox from '../../SettingsBox'
-import { ReactComponent as LinkedInSVG } from '../../../../signup/assets/linkedIn.svg'
+// import { ReactComponent as LinkedInSVG } from '../../../../signup/assets/linkedIn.svg'
 import { ReactComponent as TwitterSVG } from '../../../../signup/assets/twitter.svg'
 import { ReactComponent as GoogleSVG } from './BoxLayout/googleLog.svg'
 import BoxLayout from './BoxLayout/BoxLayout'
 import { ACCOUNT_TYPE } from 'api/user/type'
 import { useBindThirdPart } from 'bounceHooks/user/useBindThirdPart'
-import { useLinkedInOauth, useOauth } from 'state/users/hooks'
+import { useOauth } from 'state/users/hooks'
 export type ILoginOptonProps = {
   googleEmail: string
   twitter: string
-  linkedin: string
 }
 
-const LoginOpton: React.FC<ILoginOptonProps> = ({ googleEmail, twitter, linkedin }) => {
+const LoginOpton: React.FC<ILoginOptonProps> = ({ googleEmail, twitter }) => {
   const { handleOauth } = useOauth()
   const { run } = useBindThirdPart()
   const handleThirdBind = async (oauthName: string, oauthType: ACCOUNT_TYPE) => {
@@ -24,15 +23,15 @@ const LoginOpton: React.FC<ILoginOptonProps> = ({ googleEmail, twitter, linkedin
       thirdpartType: oauthType
     })
   }
-  const { linkedInLogin } = useLinkedInOauth((accessToken, oauthType) =>
-    run({
-      accessToken: accessToken,
-      thirdpartType: oauthType
-    })
-  )
-  const handleThirdLinkedinOauth = async () => {
-    linkedInLogin()
-  }
+  // const { linkedInLogin } = useLinkedInOauth((accessToken, oauthType) =>
+  //   run({
+  //     accessToken: accessToken,
+  //     thirdpartType: oauthType
+  //   })
+  // )
+  // const handleThirdLinkedinOauth = async () => {
+  //   linkedInLogin()
+  // }
 
   return (
     <SettingsBox title="Other Login Options">
@@ -42,12 +41,6 @@ const LoginOpton: React.FC<ILoginOptonProps> = ({ googleEmail, twitter, linkedin
           title={'Google account'}
           emailSvg={<GoogleSVG />}
           onBind={() => handleThirdBind('google', ACCOUNT_TYPE.GMAIL)}
-        />
-        <BoxLayout
-          email={linkedin}
-          title={'LinkedIn account'}
-          emailSvg={<LinkedInSVG />}
-          onBind={() => handleThirdLinkedinOauth()}
         />
         <BoxLayout
           email={twitter}
