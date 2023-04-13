812098b3e8a7dd7f96e1248b80584831c2e16f81
bill <>
2023-04-13 17:48:40 +0800

    fix: fix market create pool button

--- src/bounceComponents/market/components/TotalPaginationBox/index.tsx
+++ src/bounceComponents/market/components/TotalPaginationBox/index.tsx
@@ -2,11 +2,13 @@ import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
 
-import React from 'react'
+import React, { useCallback } from 'react'
 import { toast } from 'react-toastify'
 import { routes } from 'constants/routes'
-import { useUserInfo } from 'state/users/hooks'
+import { useUserInfo, useWeb3Login } from 'state/users/hooks'
 import { useNavigate } from 'react-router-dom'
+import { useActiveWeb3React } from 'hooks'
+import { useWalletModalToggle } from 'state/application/hooks'
 
 export type ITotalPaginationBoxProps = {
   total: number
@@ -17,16 +19,26 @@ export type ITotalPaginationBoxProps = {
 
 const TotalPaginationBox: React.FC<ITotalPaginationBoxProps> = ({ total, idea, children, create }) => {
   const { token } = useUserInfo()
+  const { account } = useActiveWeb3React()
+  const toggleWallet = useWalletModalToggle()
   const navigate = useNavigate()
+  const { run: web3Login } = useWeb3Login(`${routes.auction.createAuctionPool}?redirect=${routes.market.pools}`)
 
-  const handleCreateBtnClick = () => {
+  const handleCreateBtnClick = useCallback(() => {
+    if (!account) {
+      toast.error('Please connect wallet first')
+      toggleWallet()
+      return
+    }
     if (!token) {
       toast.error('Please login')
-      navigate(`${routes.login}?path=${routes.auction.createAuctionPool}?redirect=${routes.market.pools}`)
+      web3Login()
+      // navigate(`${routes.login}?path=${routes.auction.createAuctionPool}?redirect=${routes.market.pools}`)
     } else {
       navigate(`${routes.auction.createAuctionPool}?redirect=${routes.market.pools}`)
     }
-  }
+  }, [account, navigate, toggleWallet, token, web3Login])
+
   return (
     <Box p={'50px 48px 59px'} sx={{ background: '#FFFFFF', borderRadius: 20 }}>
       <Box display={'flex'} justifyContent="space-between" mb={44} alignItems={'center'}>
