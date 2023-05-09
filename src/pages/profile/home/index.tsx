f029c53e17e51c59e093fea9b133e718b64ef941
may <bugreject@gmail.com>
2023-05-10 01:30:48 +0800

    Edit: Profile

--- src/pages/profile/home/index.tsx
+++ src/pages/profile/home/index.tsx
@@ -1,7 +1,52 @@
-import ProfileHomeLayout from 'bounceComponents/profile/ProfileHomeLayout'
+import ProfileSummaryLayout, { ProfileIntroduce } from '../../../bounceComponents/profile/ProfileHomeLayout/profile'
+import { Box } from '@mui/material'
+import ProfileBg from 'assets/imgs/user/profile-bg.png'
+import { useEffect, useMemo, useState } from 'react'
+import { IProfileUserInfo } from '../../../api/user/type'
+import { getUserInfo } from '../../../api/user'
+import { useUserInfo } from '../../../state/users/hooks'
+import { useQueryParams } from '../../../hooks/useQueryParams'
 
 export default function Home() {
-  return <ProfileHomeLayout />
+  const [personalInfo, setPersonalInfo] = useState<IProfileUserInfo>()
+  const { userInfo, userId } = useUserInfo()
+  const { id } = useQueryParams()
+  const isLoginUser = useMemo(() => {
+    return Number(userId) === Number(id)
+  }, [userId, id])
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const res = await getUserInfo({ userId: Number(id) })
+      setPersonalInfo(res.data)
+    }
+    if (isLoginUser) {
+      setPersonalInfo(userInfo)
+    } else if (id) {
+      getInfo()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id, isLoginUser, userId])
+  return (
+    <Box display={'flex'} position={'relative'}>
+      <img src={ProfileBg} />
+      <Box
+        display={'flex'}
+        padding={'0 40px'}
+        gap={16}
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          minHeight: '788px',
+          top: '260px',
+          left: 0
+        }}
+      >
+        <ProfileIntroduce personalInfo={personalInfo} />
+        <ProfileSummaryLayout id={id} personalInfo={personalInfo} userId={userId} />
+      </Box>
+    </Box>
+  )
 }
 
 {
